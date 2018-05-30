cb8bf264819340f32014573a2a72c9b75b603d6d
Tommy Rogers <tommyr@TommyRogers.local>
2018-05-30 16:15:06 -0400

    completed lab

--- PersonHandler.java
+++ PersonHandler.java
@@ -1,10 +1,11 @@
- 
 
 /**
  * Created by leon on 1/24/18.
  */
 public class PersonHandler {
     private final Person[] personArray;
+    String firstName;
+    String lastName;
 
     public PersonHandler(Person[] personArray) {
         this.personArray = personArray;
@@ -12,52 +13,32 @@ public class PersonHandler {
 
     public String whileLoop() {
         String result = "";
-        // assume there is a `counter`
-        // while `counter` is less than length of array
-            // begin loop
-
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-
-            // end loop
+        int count = 0;
+        while(count < personArray.length){
+            Person identity = personArray[count];
+            result += identity.toString();
+            count++;
+        }
         return result;
     }
-
-
 
     public String forLoop() {
         String result = "";
-        // identify initial value
-        // identify terminal condition
-        // identify increment
-
-        // use the above clauses to declare for-loop signature
-            // begin loop
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
-
+        for(int i=0; i < personArray.length; i++){
+            Person identity = personArray[i];
+            result += identity.toString();
+        }
         return result;
     }
-
-
 
     public String forEachLoop() {
         String result = "";
-        // identify array's type
-        // identify array's variable-name
-
-        // use the above discoveries to declare for-each-loop signature
-            // begin loop
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
-
+        for(Person person: personArray){
+            Person identity = (person);
+            result += identity.toString();
+        }
         return result;
     }
-
 
     public Person[] getPersonArray() {
         return personArray;
